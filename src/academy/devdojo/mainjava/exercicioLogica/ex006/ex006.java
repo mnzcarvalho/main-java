0180c8ce3e53e877dfa66dfe98da5be1b5db5f00
mnzcarvalho <carvalho.mnz@gmail.com>
2025-09-26 19:39:19 -0300

    Exercícios lógica

---
+++ src/academy/devdojo/mainjava/exercicioLogica/ex006/ex006.java
@@ -0,0 +1,7 @@
+package academy.devdojo.mainjava.exercicioLogica.ex006;
+//6 - Faça um algoritmo que leia um valor qualquer e imprima na tela com um reajuste de 5%.
+public class ex006 {
+    public static void main(String[] args) {
+
+    }
+}
