a1b197e2c27c5e4d595fba829b31a372c7a44b5b
mnzcarvalho <carvalho.mnz@gmail.com>
2025-12-12 17:16:18 -0300

    Classes Utilitárias - Serialization pt 02

--- src/javacore/Xserializacao/domain/Aluno.java
+++ src/javacore/Xserializacao/domain/Aluno.java
@@ -1,12 +1,16 @@
 package javacore.Xserializacao.domain;
 
-import java.io.Serializable;
+import java.io.*;
 
 //Serializable é uma interface "FLAG", que só serve para vc testar se passa "naquele teste"
 public class Aluno implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 3190503585786412922L; //serializable class withouth (ctrl + shift + a)
     private long id;
     private String nome;
-    private String password;
+    private transient String password; // não será serializado
+    private static final String NOME_CURSO = "DevDojo Vraau"; //Atributos static pertencem a classe e não ao objeto, não será serializado
+    private transient Turma turma;
 
     public Aluno(long id, String nome, String password) {
         System.out.println("Dentro do construtor");
@@ -15,13 +19,44 @@ public class Aluno implements Serializable {
         this.password = password;
     }
 
+    @Serial
+    private void writeObject(ObjectOutputStream oos) {
+        try {
+            oos.defaultWriteObject();
+            oos.writeUTF(turma.getNome());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Serial
+    private void readObject(ObjectInputStream ois) {
+        try {
+            ois.defaultReadObject();
+            String nomeTruma = ois.readUTF();
+            turma = new Turma(nomeTruma);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public String toString() {
         return "Aluno{" +
                 "id=" + id +
                 ", nome='" + nome + '\'' +
                 ", password='" + password + '\'' +
+                ", NOME_CURSO='" + NOME_CURSO + '\'' +
+                ", Turma='" + turma + '\'' +
                 '}';
+    }
+
+    public Turma getTurma() {
+        return turma;
+    }
+
+    public void setTurma(Turma turma) {
+        this.turma = turma;
     }
 
     public long getId() {
--- src/javacore/Xserializacao/test/SerializacaoTest01.java
+++ src/javacore/Xserializacao/test/SerializacaoTest01.java
@@ -1,6 +1,7 @@
 package javacore.Xserializacao.test;
 
 import javacore.Xserializacao.domain.Aluno;
+import javacore.Xserializacao.domain.Turma;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -12,7 +13,9 @@ import java.nio.file.Paths;
 public class SerializacaoTest01 {
     public static void main(String[] args) {
         Aluno aluno1 = new Aluno(1L, "Eduardo", "123");
-//        serializar(aluno1);
+        Turma turma = new Turma("Maratona Java");
+        aluno1.setTurma(turma);
+        serializar(aluno1);
         descerealizar();
     }
 
