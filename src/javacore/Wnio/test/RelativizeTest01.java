213438aa8fd559b0607ffe1eee16329211c1f7cc
mnzcarvalho <carvalho.mnz@gmail.com>
2025-12-15 20:16:39 -0300

    Classes Utilitárias - NIO pt 05 - Relativize

---
+++ src/javacore/Wnio/test/RelativizeTest01.java
@@ -0,0 +1,27 @@
+package javacore.Wnio.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class RelativizeTest01 {
+    public static void main(String[] args) {
+        Path dir = Paths.get("/home/mnz");
+        Path clazz = Paths.get("/home/mnz/java/Curso.java");
+        Path pathToClazz = dir.relativize(clazz);
+        System.out.println(pathToClazz);
+
+
+        //caminho absoluto = raiz do sistema operacional
+        Path absoluto1 = Paths.get("/home/mnz");
+        Path absoluto2 = Paths.get("/usr/local");
+        Path absoluto3 = Paths.get("/home/mnz/java/Curso.java");
+        Path relativo1 = Paths.get("temp");
+        Path relativo2 = Paths.get("temp/temp.2021921");
+
+        System.out.println("1 " + absoluto1.relativize(absoluto3));
+        System.out.println("2 " + absoluto3.relativize(absoluto1));
+        System.out.println("3 " + absoluto1.relativize(absoluto2));
+        System.out.println("4 " + relativo1.relativize(relativo2));
+        System.out.println("5 " + absoluto1.relativize(relativo1));
+    }
+}
