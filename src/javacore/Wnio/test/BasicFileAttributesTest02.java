3c13b083890556de4f374807bbd430c8e5abfb20
mnzcarvalho <carvalho.mnz@gmail.com>
2025-12-17 23:57:53 -0300

    Classes Utilitárias - NIO pt 07 - BasicFileAttributes pt 02

---
+++ src/javacore/Wnio/test/BasicFileAttributesTest02.java
@@ -0,0 +1,31 @@
+package javacore.Wnio.test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributeView;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+
+public class BasicFileAttributesTest02 {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("home/new.txt");
+        BasicFileAttributes basicFileAttributes = Files.readAttributes(path, BasicFileAttributes.class);
+        FileTime creationTime = basicFileAttributes.creationTime();
+        FileTime lastModifiedTime = basicFileAttributes.lastModifiedTime();
+        FileTime lastAccessTime = basicFileAttributes.lastAccessTime();
+
+        System.out.println("creationTime" + creationTime);
+        System.out.println("lastModifiedTime" + lastModifiedTime);
+        System.out.println("lastAccessTime" + lastAccessTime);
+
+        BasicFileAttributeView fileAttributeView = Files.getFileAttributeView(path, BasicFileAttributeView.class);
+        FileTime newCreationTime = FileTime.fromMillis(System.currentTimeMillis());
+        fileAttributeView.setTimes(lastModifiedTime, newCreationTime, creationTime);
+
+        System.out.println("fileAttributeView" + fileAttributeView);
+        System.out.println("lastModifiedTime" + lastModifiedTime);
+        System.out.println("lastAccessTime" + lastAccessTime);
+    }
+}
