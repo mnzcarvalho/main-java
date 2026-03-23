ae13ade2c790b9042c8223bc0960efe3aa819ba4
mnzcarvalho <carvalho.mnz@gmail.com>
2026-03-23 12:15:13 -0300

    Coleções pt 07 - List pt 02

--- src/javacore/Ycolecoes/test/ListTest01.java
+++ src/javacore/Ycolecoes/test/ListTest01.java
@@ -5,23 +5,36 @@ import java.util.List;
 
 public class ListTest01 {
     public static void main(String[] args) {
-//        List nomes = new ArrayList<>(); v1.4 pode ter varios objetos de tipos diferentes.
-        List<String> nomes = new ArrayList<>(); // lista com tipo definido
+//      List nomes = new ArrayList<>(); v1.4 pode ter varios objetos de tipos diferentes.
+        List<String> nomes = new ArrayList<>(16); // lista com tipo definido (tem que ser um objeto). usar os Wrappers (Integer, Boolean...)
+        List<String> nomes2 = new ArrayList<>(16);
         nomes.add("Eduardo");
         nomes.add("List academy");
-//        nomes.add(121); ex: int na list. v1.4
+        nomes2.add("Menezes");
+        nomes2.add("List parte 2");
+        System.out.println("Retorno do equals dentro do \"remove\": " + nomes.remove("eduardo"));
 
-//        for(Object nome: nomes){    v1.4
+//      nomes.remove(0);
+//      nomes.add(121); ex: int na list. v1.4
+//      for(Object nome: nomes){    v1.4
 
+        nomes.addAll(nomes2);
         for(String nome: nomes){
             System.out.println(nome);
         }
 
         nomes.add("vrau");
         System.out.println("-------------");
-        for (int i = 0; i < nomes.size(); i++) {
-            System.out.println("Nome: " + nomes.get(i));
+        int size = nomes.size();
+        for (int i = 0; i < size; i++) {
+            System.out.printf("Nome[%s]: " + nomes.get(i) + "\n", i+1);
+            nomes.add("vrau");
         }
+
+        System.out.println("Lista completa: " + nomes);
+
+        List<Integer> numeros = new ArrayList<>();
+        numeros.add(1); //autoboxin e unboxing > conversão entre tipo primitivo e wrapper.
 
     }
 }
