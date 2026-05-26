e828bfd74fb695699696576888654fca20d65d93
mnzcarvalho <carvalho.mnz@gmail.com>
2026-05-25 23:02:38 -0300

    Coleções pt 19 - Map, HashMap, LinkedHashMap pt 02

---
+++ src/javacore/Ycolecoes/test/MapTest02.java
@@ -0,0 +1,28 @@
+package javacore.Ycolecoes.test;
+
+import javacore.Ycolecoes.domain.Consumidor;
+import javacore.Ycolecoes.domain.Manga;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapTest02 {
+    public static void main(String[] args) {
+        Consumidor consumidor1 = new Consumidor("Eduardo Menezes");
+        Consumidor consumidor2 = new Consumidor("MNZ NET ");
+
+        Manga manga1 = new Manga(02L,"Fullmetal Alchemist: Brotherhood", 22.1);
+        Manga manga2 =new Manga(01L,"Berserk", 19.9);
+        Manga manga3 =new Manga(04L,"Attack on Titan",9.9);
+        Manga manga4 =new Manga(03L,"Frieren", 11.9);
+        Manga manga5 =new Manga(05L,"Jujutsu Kaisen", 8.9);
+
+        Map<Consumidor, Manga> consumidorManga = new HashMap<>();
+        consumidorManga.put(consumidor1, manga1);
+        consumidorManga.put(consumidor2, manga4);
+
+        for (Map.Entry<Consumidor, Manga> entry : consumidorManga.entrySet()){
+            System.out.println(entry.getKey().getNome() + " - " + entry.getValue().getNome());
+        }
+    }
+}
